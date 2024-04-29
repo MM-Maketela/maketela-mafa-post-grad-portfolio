0093900161471e8bf81604b2ccfaebd6093d7e1c
Mafa Maketela <maketelamafa2@gmai.com>
2024-04-29 16:24:53 +0200

    added resume link

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,31 +1,67 @@
-import React, {useState} from 'react'
-import classes from './Header.module.css'
-import { Navigation } from '../navigation/Navigation'
+import React, { useState } from "react";
+import classes from "./Header.module.css";
+import { Navigation } from "../navigation/Navigation";
 import { MdWhatsapp, MdFacebook } from "react-icons/md";
 import { AiOutlineLinkedin, AiOutlineGithub } from "react-icons/ai";
+import { FaExternalLinkAlt } from "react-icons/fa";
 export const Header = () => {
-
-const [changeColor, setChangeColor] = useState(false)
-function windowScrolling(){
-    window.scrollY >= 1 ? setChangeColor(true): setChangeColor(false)
-}
- window.addEventListener("scroll", windowScrolling);
+  const [changeColor, setChangeColor] = useState(false);
+  function windowScrolling() {
+    window.scrollY >= 1 ? setChangeColor(true) : setChangeColor(false);
+  }
+  window.addEventListener("scroll", windowScrolling);
   return (
-    <header className={classes.header} style={{backgroundColor:changeColor? "rgb(96, 125, 139)":"inherit"}}>
+    <header
+      className={classes.header}
+      style={{ backgroundColor: changeColor ? "rgb(96, 125, 139)" : "inherit" }}
+    >
       <div className={classes.contactInfo}>
-            <ul className={classes.contactList}>
-              <li >< MdWhatsapp  size={30}   className={classes.contactListItem}/> </li>
-              <li >< MdFacebook  size={30} className={classes.contactListItem}/> </li>
-              <li >< AiOutlineLinkedin  size={30} className={classes.contactListItem}/> </li>
-              <li > <AiOutlineGithub  size={30} className={classes.contactListItem}/>  </li>
-              
-            </ul>
+        <ul className={classes.contactList}>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://drive.google.com/file/d/1m34lrXT1ojd5l89aUajqcPbW-wMmnI2i/view?usp=sharing"
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: ".3rem",
+              border: "8px solid rgba(255, 255, 255, 0.05)",
+              padding: ".3rem",
+              color:"white"
+            }}
+          >
+            <h4 style={{ padding: "0px", margin: "0px" }}>RESUME</h4>
+            <div>
+              <FaExternalLinkAlt />
+            </div>
+          </a>
+          <li>
+            <MdWhatsapp size={20} className={classes.contactListItem} />{" "}
+          </li>
+          <li>
+            <MdFacebook size={20} className={classes.contactListItem} />{" "}
+          </li>
+          <li>
+            <AiOutlineLinkedin size={20} className={classes.contactListItem} />{" "}
+          </li>
+          <li>
+            {" "}
+            <AiOutlineGithub
+              size={20}
+              className={classes.contactListItem}
+            />{" "}
+          </li>
+        </ul>
       </div>
-      <div className={classes.divider}></div>
-      <div style={{position:"relative"}}>
-      <Navigation />      
 
+      <div style={{ display: "flex" }}>
+        <div style={{ flexGrow: "1" }}></div>
+        <div className={classes.divider}></div>
+      </div>
+
+      <div style={{ position: "relative" }}>
+        <Navigation />
       </div>
     </header>
-  )
-}
+  );
+};
