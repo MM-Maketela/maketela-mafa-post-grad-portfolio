7a610c40ea658829db53734d2c7b149fe2924563
Mafa Maketela <maketelamafa2@gmai.com>
2024-04-30 12:48:05 +0200

    modified serviceCard

--- src/components/service-card/ServiceCard.tsx
+++ src/components/service-card/ServiceCard.tsx
@@ -3,15 +3,15 @@ import classes from './ServiceCard.module.css'
 
 interface ServiceCardProps {
   heading1:string,
-  text1:string,
+  text1:[],
   Icon?: ReactNode
 
 }
 
+
 export const ServiceCard:React.FC<ServiceCardProps> = ({heading1, text1, Icon}) => {
   const [headerText] = useState(heading1)
   const [text] = useState(text1)
-
   return (
     <div className={classes.serviceCard}>
 
@@ -20,12 +20,15 @@ export const ServiceCard:React.FC<ServiceCardProps> = ({heading1, text1, Icon}) 
       </div>
 
       <div>
-      <h1  style={{fontSize:'1.5rem'}}>
+      <h1  style={{fontSize:'1.5rem', textAlign:"center"}}>
         {headerText.toUpperCase()}
       </h1>
-      <p style={{fontSize:'1.1rem'}}>
-    {text}
-      </p>
+      <div >
+    <ul style={{margin:"1rem 0rem 0rem 0rem", padding:"0px" , listStyle:"none", display:"grid", gap:".2rem", fontSize:"1.2rem", color:"rgba(0, 128, 128)"}}>
+     
+     {text1?.map((element:string, index:number):ReactNode => (<li id={index +""}>{element}</li>))}
+    </ul>
+      </div>
       </div>
      
 
--- src/pages/home/sections/services/Services.jsx
+++ src/pages/home/sections/services/Services.jsx
@@ -3,6 +3,11 @@ import classes from './Services.module.css'
 import { ServiceCard } from '../../../../components/service-card/ServiceCard'
 import { BiTask } from "react-icons/bi"
 import { SubHeader } from '../../../../components/sub-header/SubHeader'
+import { FaReact,FaNodeJs, FaJava ,IoLogoJavascript, FaAddressCard  } from "react-icons/fa";
+import { SiAndroidstudio } from "react-icons/si"
+import { LiaChalkboardTeacherSolid } from "react-icons/lia";
+import { TbReportAnalytics } from "react-icons/tb";
+
 export const Services = () => {
   const size = 80
   return (
@@ -13,12 +18,12 @@ export const Services = () => {
 
         <div className={classes.servicesCards}>
 
-            <ServiceCard Icon = {< BiTask size={size}/>} text1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident. " heading1 = "heading"/>
-            <ServiceCard Icon = {< BiTask size={size}/>} text1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident. " heading1 = "heading"/>
-            <ServiceCard Icon = {< BiTask size={size}/>} text1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident. " heading1 = "heading"/>
-            <ServiceCard Icon = {< BiTask size={size}/>} text1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident. " heading1 = "heading"/>
-            <ServiceCard Icon = {< BiTask size={size}/>} text1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident. " heading1 = "heading"/>
-            <ServiceCard Icon = {< BiTask size={size}/>} text1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni adipisci eaque autem fugiat! Quia, provident vitae! Magni tempora perferendis eum non provident. " heading1 = "heading"/>
+            <ServiceCard Icon = {< FaReact size={size}/>} text1= {['Data Collection','Planing', 'Design', 'Agile Implementation']}  heading1 = "frontend web-dev"/>
+            <ServiceCard Icon = {< FaNodeJs size={size}/>} text1= {['Data Collection','Planing', 'Design', 'Agile Implementation']} heading1 = "backend web-dev"/>
+            <ServiceCard Icon = {< SiAndroidstudio size={size}/>} text1= {['Data Collection','Planing', 'Design', 'Agile Implementation']} heading1 = "android app-dev"/>
+            <ServiceCard Icon = {< TbReportAnalytics size={size}/>} text1= {['Data Collection', 'Isolate Problem', 'Find Root Cause', 'Brainstorm Solutions', 'Implement Solution']} heading1 = "data analysis"/>
+            <ServiceCard Icon = {< FaAddressCard size={size}/>} text1= {['Data Collection', 'Compile Resume', 'Compile Cover Letter', 'Format Docs As PDF']} heading1 = "compile a resume"/>
+            <ServiceCard Icon = {< LiaChalkboardTeacherSolid size={size}/>} text1= {['Explain Concepts', 'Test Student', 'Provide Feedback', 'Share Advice', 'Mentor']} heading1 = "grade12 maths tutor"/>
 
         </div>
 
