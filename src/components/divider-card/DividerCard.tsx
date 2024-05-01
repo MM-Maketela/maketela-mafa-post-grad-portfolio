7bf97d289f05f80eb1a6cc0d2bdd06490f58018d
Mafa Maketela <maketelamafa2@gmai.com>
2024-05-01 18:25:39 +0200

    completed the divider

--- src/components/divider-card/DividerCard.tsx
+++ src/components/divider-card/DividerCard.tsx
@@ -1,21 +1,20 @@
 import React, { ReactNode } from "react";
 import classes from "./DividerCard.module.css";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 
 interface DividerCardProps {
-  text:string,
-  number: number,
-  icon?: ReactNode
+  text: string;
+  number: number;
+  icon?: ReactNode;
 }
 
-
-export const DividerCard:React.FC<DividerCardProps> = ({text,number, icon}) => {
-
+export const DividerCard: React.FC<DividerCardProps> = ({ text, number, icon }) => {
+  const [ref, inView] = useInView();
   return (
     <div className={classes.dividerCard}>
-      <div className={classes.iconContainer}>
-        {icon}
-      </div>
-      <h4 style={{ fontSize: "1.5rem", margin: "0px" }}>{number}</h4>
+      <div className={classes.iconContainer}>{icon}</div>
+      <h4 style={{ fontSize: "1.5rem", margin: "0px" }}>{<div ref={ref}>{inView ? <CountUp start={-50} duration={2} end={number} /> : number}</div>}</h4>
       <p style={{ color: "white", margin: "0px" }}>{text.toUpperCase()}</p>
     </div>
   );
--- src/pages/home/sections/divider-section/DividerSection.tsx
+++ src/pages/home/sections/divider-section/DividerSection.tsx
@@ -1,32 +1,30 @@
 import React from "react";
 import classes from "./DividerSection.module.css";
 import { DividerCard } from "../../../../components/divider-card/DividerCard";
-import { RiTimer2Line } from "react-icons/ri";
+import { IoMdTime } from "react-icons/io";
+import { RiTeamLine } from "react-icons/ri";
+import { LiaProjectDiagramSolid } from "react-icons/lia";
 const size = 50;
 
 export const DividerSection = () => {
   return (
     <div className={classes.dividerSection}>
       <DividerCard
-        text="amet consectetur"
-        number={45}
-        icon={<RiTimer2Line size={size} />}
+        text="Projects contributed in"
+        number={8}
+        icon={<LiaProjectDiagramSolid size={size} />}
       />
       <DividerCard
-        text="amet consectetur"
-        number={45}
-        icon={<RiTimer2Line size={size} />}
+        text="years of experience"
+        number={2}
+        icon={<IoMdTime size={size} />}
       />
       <DividerCard
-        text="amet consectetur"
-        number={45}
-        icon={<RiTimer2Line size={size} />}
+        text="Team Collaboration"
+        number={3}
+        icon={<RiTeamLine size={size} />}
       />
-      <DividerCard
-        text="amet consectetur"
-        number={45}
-        icon={<RiTimer2Line size={size} />}
-      />
+      
     </div>
   );
 };
